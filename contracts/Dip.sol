eb1f7870728f6cf6ee1231ec28d0a98ff6dc8d9c
pemulis <john.d.shutt@gmail.com>
2020-10-03 14:50:54 -0700

    feat: simplify check for whether share has been calculated postdip

--- contracts/Dip.sol
+++ contracts/Dip.sol
@@ -14,8 +14,8 @@ contract Dip is ERC20 {
   private address _dipPair;
 
   private bool _predip = true;
+  private uint256 _startTime;
   private uint256 _initialSupply;
-  private mapping (address => bool) _shareCalculated;
   private mapping (address => uint256) _shares;
   private mapping (address => mapping (address => uint256))  _shareAllowances;
 
@@ -35,6 +35,7 @@ contract Dip is ERC20 {
       keccak256(abi.encodePacked(address(this), baseToken)),
       hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
     ))));
+    _startTime = block.timestamp;
   }
 
   function balanceOf(address account) public view override returns (uint256) {
@@ -82,7 +83,7 @@ contract Dip is ERC20 {
       _approve(_msgSender(), spender, amount);
       return true;
     }
-    if (shareCalculated[_msgSender()] === false) {
+    if (_balances[_msgSender()] > 0) {
       _calculateShare(_msgSender());
     }
     amount = _convertAmountToShare(amount);
@@ -95,7 +96,7 @@ contract Dip is ERC20 {
       _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "ERC20: transfer amount exceeds allowance"));
       return true;
     }
-    if (shareCalculated[sender] === false) {
+    if (_balances[sender] > 0) {
       _calculateShare(sender);
     }
     amount = _convertAmountToShare(amount);
@@ -109,7 +110,7 @@ contract Dip is ERC20 {
       _approve(_msgSender(), spender, _allowances[_msgSender()][spender].add(addedValue));
       return true;
     }
-    if (shareCalculated[_msgSender()] === false) {
+    if (_balances[_msgSender()] > 0) {
       _calculateShare(_msgSender());
     }
     addedValue = _convertAmountToShare(addedValue);
@@ -121,17 +122,13 @@ contract Dip is ERC20 {
       _approve(_msgSender(), spender, _allowances[_msgSender()][spender].sub(subtractedValue, "ERC20: decreased allowance below zero"));
       return true;
     }
-    if (shareCalculated[_msgSender()] === false) {
+    if (_balances[_msgSender()] > 0) {
       _calculateShare(_msgSender());
     }
     subtractedValue = _convertAmountToShare(subtractedValue);
     _shareAllowances[_msgSender(), spender].sub(subtractedValue);
   }
 
-  /* Dip specific functions */
-
-  // user swaps target token during distribution period for dip tokens
-  // reject ERC20 transfers unless target token and in predip period?
   function swap(uint256 amount) public {
     require(_predip === true, "The token distribution period is over.");
     IERC20(_targetToken).transferFrom(_msgSender(), address(this), amount);
@@ -140,6 +137,7 @@ contract Dip is ERC20 {
   }
 
   function dip() public {
+    require(block.timestamp >= _startTime.add(2 days), "Token distribution lasts for two days before the dip.");
     _predip = false;
     _calculateShare(_dipPair);
   }
@@ -147,12 +145,11 @@ contract Dip is ERC20 {
   // set a user's share relative to their percentage of the initial total supply
   function _calculateShare(address account) internal {
     require(_predip === false, "The token distribution period is still active.");
-    require(shareCalculated[account] === false, "Token share has already been calculated for this account.");
+    require(_balances[account] === 0, "Token share has already been calculated for this account.");
     uint256 share = FixedPoint.calculateMantissa(
       _balances[account],
       _initialSupply
     );
-    shareCalculated[account] = true;
     delete _balances[account];
     _shares[account] = share;
   }
